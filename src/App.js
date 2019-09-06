23f96b92b072963b1f1b1b3c74c3060bc6fc50db
Chirag Wadhwa <chiragw34@gmail.com>
2019-09-06 17:59:47 +0530

    adding front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+// Components
+import Navbar from "./components/Navbar";
+
+// Pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+const theme = createMuiTheme({
+  palette: {
+    common: {
+      black: "#fff",
+      white: "rgba(0, 0, 0, 1)"
+    },
+    background: {
+      paper: "rgba(39, 0, 47, 1)",
+      default: "rgba(0, 0, 0, 1)"
+    },
+    primary: {
+      light: "rgba(207, 112, 255, 1)",
+      main: "rgba(189, 16, 224, 1)",
+      dark: "rgba(144, 19, 254, 1)",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "rgba(207, 112, 255, 1)",
+      main: "rgba(189, 16, 224, 1)",
+      dark: "rgba(144, 19, 254, 1)",
+      contrastText: "#fff"
+    },
+    error: {
+      light: "#e57373",
+      main: "rgba(255, 0, 0, 1)",
+      dark: "#d32f2f",
+      contrastText: "#fff"
+    },
+    text: {
+      primary: "#fff",
+      secondary: "rgba(189, 16, 224, 1)",
+      disabled: "rgba(0, 0, 0, 0.38)",
+      hint: "rgba(126, 211, 33, 1)"
+    },
+    typography: {
+      useNextVariants: true
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
